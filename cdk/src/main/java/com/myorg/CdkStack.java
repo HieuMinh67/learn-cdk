c2bca25f6b111fd8ac3ca71c1160b6a4ecf61acc
Henry Pham <henry.pham@gft.com>
2022-06-22 14:55:42 +0700

    init

---
+++ cdk/src/main/java/com/myorg/CdkStack.java
@@ -0,0 +1,71 @@
+package com.myorg;
+
+import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.apigateway.*;
+import software.amazon.awscdk.services.apigateway.IResource;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.FunctionProps;
+import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.s3.assets.AssetOptions;
+import software.constructs.Construct;
+
+import java.util.Arrays;
+import java.util.List;
+import static java.util.Collections.singletonList;
+
+public class CdkStack extends Stack {
+    public CdkStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public CdkStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        List<String> functionPackagingInstructions = Arrays.asList(
+                "/bin/sh",
+                "-c",
+                "cd HealthCheck " +
+                        "&& mvn clean install " +
+                        "&& cp /asset-input/HealthCheck/target/health-checker.jar /asset-output/"
+        );
+
+        BundlingOptions.Builder builderOptions = BundlingOptions.builder()
+                .command(functionPackagingInstructions)
+                .image(Runtime.JAVA_11.getBundlingImage())
+                .volumes(singletonList(
+                        DockerVolume.builder()
+                                .hostPath(System.getProperty("user.home") + "/.m2/")
+                                .containerPath("/root/.m2/")
+                                .build()
+                ))
+                .user("root")
+                .outputType(BundlingOutput.ARCHIVED);
+
+        Function checkHealthFunction = new Function(
+                this,
+                "HealthCheck",
+                FunctionProps.builder()
+                        .code(Code.fromAsset("../lambda", AssetOptions.builder()
+                                .bundling(builderOptions
+                                        .command(functionPackagingInstructions)
+                                        .build()
+                                ).build()
+                        ))
+                        .handler("com.henry.App")
+                        .runtime(Runtime.JAVA_11)
+                        .timeout(Duration.seconds(30))
+                        .memorySize(512)
+                        .build()
+        );
+
+        RestApi api = new RestApi(this, "healthApi",
+                RestApiProps.builder().restApiName("Server's Health Service").build());
+
+        IResource items = api.getRoot().addResource("health");
+
+        Integration healthIntegration = new LambdaIntegration(checkHealthFunction);
+        items.addMethod("GET", healthIntegration);
+
+    }
+}
---
+++ lambda/HealthCheck/src/main/java/com/henry/App.java
@@ -0,0 +1,25 @@
+package com.henry;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>
+{
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-type", "text/plain");
+        headers.put("X-Custom-Header", "text/plain");
+
+        return new APIGatewayProxyResponseEvent()
+                .withHeaders(headers)
+                .withStatusCode(200)
+                .withBody("Good");
+    }
+
+}
